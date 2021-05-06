66f47ac9e46af626fbe6e1038fb22e5814c53f6a
Antônio Ferreira <antonio.carlos.o.ferreira@gmail.com>
2021-05-06 17:53:47 -0300

    Trying to solve some convolution problems due to weights.

--- resnet/test.py
+++ resnet/test.py
@@ -25,20 +25,20 @@ outgen = datagen.flow_from_directory(
 
 traingen = datagen.flow_from_directory(
     #directory="/home/antonio/Imagens/gray/",
-    #directory="/home/antonio/Downloads/dog vs cat/dataset/test_set/",
-    directory="/home/antonio/Imagens/labAdr/",
+    directory="/home/antonio/Downloads/dog vs cat/dataset/test_set/",
+    #directory="/home/antonio/Imagens/labAdr/",
     class_mode='binary',
     color_mode='grayscale',
-    batch_size=1,
+    batch_size=16,
     subset='training',
     target_size=(224,224))
 
 testgen = datagen.flow_from_directory(
-    #directory="/home/antonio/Downloads/dog vs cat/dataset/test_set/",
-    directory="/home/antonio/Imagens/labAdr/",
+    directory="/home/antonio/Downloads/dog vs cat/dataset/test_set/",
+    #directory="/home/antonio/Imagens/labAdr/",
     class_mode='binary',
     color_mode='grayscale',
-    batch_size=1,
+    batch_size=16,
     subset='validation',
     target_size=(224,224))
 
@@ -47,64 +47,103 @@ inputs = layers.Input(shape=(224, 224, 1))
 # Layer 1
 x = layers.ZeroPadding2D((3,3))(inputs)
 x = layers.Conv2D(64, (7, 7), (2, 2), padding='valid', use_bias=False, name='conv1')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 
 # Layer 2
 x = layers.ZeroPadding2D((1,1))(x)
 x1 = layers.MaxPooling2D((3, 3), strides=(2,2), padding='valid')(x)
 x = layers.ZeroPadding2D((1,1))(x1)
 x = layers.Conv2D(64, (3, 3), padding='valid', use_bias=False, name='conv2_1a')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x2 = layers.Conv2D(64, (3, 3), padding='valid', use_bias=False, name='conv2_2a')(x)
+x = layers.Conv2D(64, (3, 3), padding='valid', use_bias=False, name='conv2_2a')(x)
+x = layers.BatchNormalization()(x)
+x2 = layers.ReLU()(x)
 added = layers.add([x1,x2])
 x = layers.ZeroPadding2D((1,1))(added)
 x = layers.Conv2D(64, (3, 3), padding='valid', use_bias=False, name='conv2_1b')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x3 = layers.Conv2D(64, (3, 3), padding='valid', use_bias=False, name='conv2_2b')(x)
+x = layers.Conv2D(64, (3, 3), padding='valid', use_bias=False, name='conv2_2b')(x)
+x = layers.BatchNormalization()(x)
+x3 = layers.ReLU()(x)
 added = layers.add([added,x3])
 
 # Layer 3
 
 sc = layers.Conv2D(128, (1, 1), (2, 2), padding='valid', use_bias=False, name='shortcut3')(added)
+sc = layers.BatchNormalization()(sc)
+sc = layers.ReLU()(sc)
 x = layers.ZeroPadding2D((1, 1))(added)
 x = layers.Conv2D(128, (3, 3), (2, 2), padding='valid', use_bias=False, name='conv3_1a')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x2 = layers.Conv2D(128, (3, 3), padding='valid', use_bias=False, name='conv3_2a')(x)
+x = layers.Conv2D(128, (3, 3), padding='valid', use_bias=False, name='conv3_2a')(x)
+x = layers.BatchNormalization()(x)
+x2 = layers.ReLU()(x)
 added = layers.add([sc,x2])
 x = layers.ZeroPadding2D((1,1))(added)
 x = layers.Conv2D(128, (3, 3), padding='valid', use_bias=False, name='conv3_1b')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x3 = layers.Conv2D(128, (3, 3), padding='valid', use_bias=False, name='conv3_2b')(x)
+x = layers.Conv2D(128, (3, 3), padding='valid', use_bias=False, name='conv3_2b')(x)
+x = layers.BatchNormalization()(x)
+x3 = layers.ReLU()(x)
 added = layers.add([added,x3])
 x = layers.ZeroPadding2D((1,1))(added)
 
 # Layer 4
 
 sc = layers.Conv2D(256, (1, 1), (2, 2), padding='valid', use_bias=False, name='shortcut4')(added)
+sc = layers.BatchNormalization()(sc)
+sc = layers.ReLU()(sc)
 x = layers.ZeroPadding2D((1, 1))(added)
 x = layers.Conv2D(256, (3, 3), (2, 2), padding='valid', use_bias=False, name='conv4_1a')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x2 = layers.Conv2D(256, (3, 3), padding='valid', use_bias=False, name='conv4_2a')(x)
+x = layers.Conv2D(256, (3, 3), padding='valid', use_bias=False, name='conv4_2a')(x)
+x = layers.BatchNormalization()(x)
+x2 = layers.ReLU()(x)
 added = layers.add([sc,x2])
 
 x = layers.ZeroPadding2D((1,1))(added)
 x = layers.Conv2D(256, (3, 3), padding='valid', use_bias=False, name='conv4_1b')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x3 = layers.Conv2D(256, (3, 3), padding='valid', use_bias=False, name='conv4_2b')(x)
+x = layers.Conv2D(256, (3, 3), padding='valid', use_bias=False, name='conv4_2b')(x)
+x = layers.BatchNormalization()(x)
+x3 = layers.ReLU()(x)
 added = layers.add([added,x3])
 x = layers.ZeroPadding2D((1,1))(added)
 
 # Layer 5
 
 sc = layers.Conv2D(512, (1, 1), (2, 2), padding='valid', use_bias=False, name='shortcut5')(added)
+sc = layers.BatchNormalization()(sc)
+sc = layers.ReLU()(sc)
 x = layers.ZeroPadding2D((1, 1))(added)
 x = layers.Conv2D(512, (3, 3), (2, 2), padding='valid', use_bias=False, name='conv5_1a')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x2 = layers.Conv2D(512, (3, 3), padding='valid', use_bias=False, name='conv5_2a')(x)
+x = layers.Conv2D(512, (3, 3), padding='valid', use_bias=False, name='conv5_2a')(x)
+x = layers.BatchNormalization()(x)
+x2 = layers.ReLU()(x)
 added = layers.add([sc,x2])
 x = layers.ZeroPadding2D((1,1))(added)
 x = layers.Conv2D(512, (3, 3), padding='valid', use_bias=False, name='conv5_1b')(x)
+x = layers.ReLU()(x)
 x = layers.ZeroPadding2D((1,1))(x)
-x3 = layers.Conv2D(512, (3, 3), padding='valid', use_bias=False, name='conv5_2b')(x)
+x = layers.Conv2D(512, (3, 3), padding='valid', use_bias=False, name='conv5_2b')(x)
+x = layers.BatchNormalization()(x)
+x3 = layers.ReLU()(x)
 added = layers.add([added,x3])
 x = layers.ZeroPadding2D((1,1))(added)
 
@@ -113,7 +152,7 @@ x = layers.ZeroPadding2D((1,1))(added)
 output = layers.AveragePooling2D((7,7))(added)
 flat = layers.Flatten()(output)
 result = layers.Dense(1, use_bias=False)(flat)
-#result = layers.Activation("sigmoid")(output)
+result = layers.Activation("sigmoid")(result)
 model = models.Model(inputs=inputs, outputs=result)
 
 model_filepath = "results/modelbw-{epoch:04d}.h5"
@@ -121,7 +160,7 @@ model_filepath = "results/modelbw-{epoch:04d}.h5"
 model_checkpoint = ModelCheckpoint(
     filepath=model_filepath,
     verbose=1,
-    period=1)
+    period=50)
 
 csv_logger = CSVLogger("logs/test.csv")
 
@@ -131,12 +170,12 @@ model.fit(
     traingen,
     callbacks=[model_checkpoint, csv_logger],
     validation_data=testgen,
-    validation_steps=testgen.samples//1,
-    epochs=10,
-    steps_per_epoch=traingen.samples//1)
+    validation_steps=testgen.samples//16,
+    epochs=750,
+    steps_per_epoch=traingen.samples//16)
 
 model.summary()
-
+'''
 w_file1 = open("weights-conv1.txt", "w")
 w_file2 = open("weights-conv2.txt", "w")
 w_file3 = open("weights-conv3.txt", "w")
@@ -148,13 +187,19 @@ first = True
 
 n = 0
 fully = 0
-string_total = str()
+conv1 = 0
+string_1 = list()
+string_2 = list()
+string_3 = list()
+string_4 = list()
+string_5 = list()
+string_fully = list()
 
 for layer in model.layers:
     weights = layer.get_weights()
     
     if len(weights) is not 0:
-
+        print("Entering the loop")
         for line in weights:
             for column in line:
                 try:
@@ -163,96 +208,84 @@ for layer in model.layers:
                             for element in conv_filter:
                                 if first:
                                     if n == 0:
-                                        w_file1.write(str(element))
+                                        string_1.append(str(element))
                                     elif n >= 1 and n <= 4:
-                                        w_file2.write(str(element))
+                                        string_2.append(str(element))
+                                        w_file2.write(string_2)
                                     elif n >= 5 and n <= 9:
-                                        w_file3.write(str(element))
+                                        string_3 = str(element)
+                                        w_file3.write(string_3)
                                     elif n >= 10 and n <=14:
-                                        w_file4.write(str(element))
+                                        string_4 = str(element)
+                                        w_file4.write(string_4)
                                     elif n >= 15 and n <= 19:
-                                        w_file5.write(str(element))
-                                    #elif n == 20:
-                                        #w_filefully.write(str(element))
-                                        #fully = fully + 1
-                                    
+                                        string_5 = str(element)
+                                        w_file5.write(string_5)
+
                                     w_file.write(str(element))
                                     first = False
                                 else:
                                     if n == 0:
-                                        w_file1.write("*" + str(element))
+                                        string_1 = "*" + str(element)
+                                        w_file1.write(string_1)
                                     elif n >= 1 and n <= 4:
-                                        w_file2.write("*" + str(element))
+                                        string_2 = "*" + str(element)
+                                        w_file2.write(string_2)
                                     elif n >= 5 and n <= 9:
-                                        w_file3.write("*" + str(element))
+                                        string_3 = "*" + str(element)
+                                        w_file3.write(string_3)
                                     elif n >= 10 and n <=14:
-                                        w_file4.write("*" + str(element))
+                                        string_4 = "*" + str(element)
+                                        w_file4.write(string_4)
                                     elif n >= 15 and n <= 19:
-                                        w_file5.write("*" + str(element))
-                                    #elif n == 20:
-                                        #w_filefully.write("*" + str(element))
-                                        #fully = fully + 1
+                                        string_5 = "*" + str(element)
+                                        w_file5.write(string_5)
                                     
                                     w_file.write("*" + str(element))
                 except:
                     for element in column:
                         if first:
-                            if n == 0:
-                                w_file1.write(str(element))
-                            elif n >= 1 and n <= 4:
-                                w_file2.write(str(element))
-                            elif n >= 5 and n <= 9:
-                                w_file3.write(str(element))
-                            elif n >= 10 and n <=14:
-                                w_file4.write(str(element))
-                            elif n >= 15 and n <= 19:
-                                w_file5.write(str(element))
-                            elif n == 20:
-                                #w_filefully.write(str(element))
-                                #print(element)
-                                string_total = "*" + str(element)
+                            if n == 20:
+                                string_fully = "*" + str(element)
+                                w_filefully.write(string_fully)
                                 fully = fully + 1
 
                             w_filefully.write(str(element))
                             first = False
                         else:
-                            if n == 0:
-                                w_file1.write("*" + str(element))
-                            elif n >= 1 and n <= 4:
-                                w_file2.write("*" + str(element))
-                            elif n >= 5 and n <= 9:
-                                w_file3.write("*" + str(element))
-                            elif n >= 10 and n <=14:
-                                w_file4.write("*" + str(element))
-                            elif n >= 15 and n <= 19:
-                                w_file5.write("*" + str(element))
-                            elif n == 20:
-                                #w_filefully.write("*" + str(element))
-                                #print(element)
-                                string_total = string_total + "*" + str(element)
+                            if n == 20:
+                                string_fully = "*" + str(element)
+                                w_filefully.write(string_fully)
                                 fully = fully + 1
                             
                             w_file.write("*" + str(element))
-        w_filefully.write(string_total)
+
         if n == 0:
+            #w_file1.write(string_1)
             w_file1.write("\n")
         elif n >= 1 and n <= 4:
+            #w_file2.write(string_2)
             w_file2.write("\n")
         elif n >= 5 and n <= 9:
+            #w_file3.write(string_3)
             w_file3.write("\n")
         elif n >= 10 and n <= 14:
+            #w_file4.write(string_4)
             w_file4.write("\n")
         elif n >= 15 and n <= 19:
+            #w_file5.write(string_5)
             w_file5.write("\n")
         elif n == 20:
+            #w_filefully.write(string_fully)
             w_filefully.write("\n")
 
         w_file.write("\n")
         first = True
         n = n + 1
+        print("Exiting the loop")
     
 print(fully)
-
+'''
 model.save("weights.h5")
-out = model.predict(outgen)
-print(out)
+#out = model.predict(outgen)
+#print(out)
